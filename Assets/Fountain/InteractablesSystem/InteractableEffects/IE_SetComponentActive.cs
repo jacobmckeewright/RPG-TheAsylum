c4cf372bfb8b085e21f33435fe24afa089446e14
Connor <clocke555@gmail.com>
2021-08-08 14:19:28 +1000

    Added Effect to turn off and on components

---
+++ Assets/Fountain/InteractablesSystem/InteractableEffects/IE_SetComponentActive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IE_SetComponentActive : InteractableEffect
+{
+
+    [SerializeField]
+    [Tooltip("MonoBehaviour to set the enabled state to")]
+    private MonoBehaviour targetComponent;
+
+    [SerializeField]
+    [Tooltip("State to which we are changing")]
+    bool state = false;
+
+    public override void Fire()
+    {
+        if (targetComponent != null)
+            targetComponent.enabled = state;
+        else
+            Debug.LogWarning("[Interactable] [SetComponentActive] targetComponent is null");
+    }
+}
