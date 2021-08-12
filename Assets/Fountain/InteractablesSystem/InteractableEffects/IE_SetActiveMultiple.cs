b2e40053714b5d3bca4c2a374b0bd5e413f5d476
Connor Locke-Warburton <clocke555@gmail.com>
2021-08-12 15:47:18 +1000

    Added IE to Set Multiple objects active

---
+++ Assets/Fountain/InteractablesSystem/InteractableEffects/IE_SetActiveMultiple.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IE_SetActiveMultiple : InteractableEffect
+{
+    [SerializeField]
+    [Tooltip("Target Gameobjects to setActive")]
+    GameObject[] targetObjects = null;
+
+    [SerializeField]
+    [Tooltip("State to which we are changing")]
+    bool state = false;
+
+    public override void Fire()
+    {
+        if (targetObjects != null)
+        {
+            for (int i = 0; i < targetObjects.Length; i++)
+            {
+                if (targetObjects[i] != null)
+                    targetObjects[i].SetActive(state);
+            }
+        }
+        else
+            Debug.LogWarning("[Interactable] [SetActiveMultiple] targetObjects array is null");
+    }
+}
